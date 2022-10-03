94e3f635bd6ebd7b6b8ef7741ef8dcfe49cc2ab4
Jaume Sánchez <jasato89@gmail.com>
2022-10-03 15:51:20 +0200

    Latest changes

--- university.sql
+++ university.sql
@@ -68,6 +68,68 @@ select * from grades where score < 60;
 
 select * from grades where student_name LIKE '%S%';
 
+-- ejercicios de clase
+
+/*
+Use aggregation functions to find:
+
+1. the average grade by student.
+2. the total number of courses taken by each student
+3. the minimum grade by section
+4. the maximum grade by section
+5. the total number of courses taken by a student who has more than 1 course taken
+*/
+
+-- 1
+select student_name as Student, AVG(score) AS Average from grades group by student_name;
+
+-- 2
+select student_name, COUNT(section_id) AS "# of courses" from grades group by student_name;
+
+-- 3 
+select section_id, MIN(score) AS "Min score" from grades group by section_id;
+
+-- 4
+
+select section_id, MAX(score) AS "Max score" from grades group by section_id;
+
+-- 5
+select student_name, COUNT(section_id) AS "# of courses" from grades group by student_name having COUNT(section_id) > 1;
+
+-- LIKE: No exacto pero sí se cumple algún criterio
+
+SELECT * FROM grades WHERE section_id LIKE '%-A';
+
+-- BETWEEN: Un rango de datos
+SELECT * FROM grades where score BETWEEN 80 AND 90 OR student_name LIKE "%om%";
+SELECT distinct student_name FROM grades where student_name BETWEEN 'L' AND 'R';
+
+-- DISTINCT: nos muestra una sola muestra del dato, no los repetidos
+
+select distinct student_name from grades;
+
+select distinct student_name, score from grades;
+
+-- ORDER BY: para ordenar
+
+select student_name, score from grades ORDER BY score DESC;
+
+select * from grades;
+
+
+
+SELECT student_name, score, professor FROM grades LEFT JOIN section ON grades.section_id = section.id;
+
+SELECT g.student_name, g.score FROM grades g 
+JOIN section s ON g.section_id = s.id
+JOIN course ON s.course_code = course.course_code WHERE course_name = "Databases";
+
+
+
+
+
+
+
 
     
     
